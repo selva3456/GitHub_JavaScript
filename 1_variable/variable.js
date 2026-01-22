3d4f0b11cc763208d4b835b81b6f4f3ea6f91853
Sriselvasakthimaari_021 <ndhurairaj1967@gmail.com>
2026-01-22 13:51:08 +0530

    variable.html and js added

---
+++ 1_variable/variable.js
@@ -0,0 +1,54 @@
+var a
+console.log(a);
+ a=100;
+console.log(a);
+
+let b=100
+    b=200
+    console.log(b);
+
+const c=500
+    console.log(c);
+
+    var n
+    console.log(n)
+
+    let f
+    console.log(f)
+
+// scope 
+// 1.global scope->outside the function or block
+// 2.block scope ->within the block{}
+// 3.function scope or local scope ->within the function 
+
+
+var a=100;
+let b1=200
+const c1=300
+console.log("this area is global scope")
+    console.log(a,b1,c1)
+
+{
+    var d=10;//global scope variable
+    let e=20;//block scope variable
+    const f=30;//block scope variable
+    console.log("this is block scope area")
+     console.log(a,b,c)
+     console.log(d,e,f)
+}
+
+//var will not follow block scope
+     console.log(d)
+    //  console.log(e)//Uncaught ReferenceError: e is not definedc
+    //  console.log(f)//Uncaught ReferenceError: f is not defined
+abc()
+function abc(){
+    var x=1000
+    let y=2000
+    const z=3000
+    console.log("this is function scope or local scope area")
+    console.log(a,b,c)
+    console.log(x,y,z)
+    console.log(x,y)
+}
+
